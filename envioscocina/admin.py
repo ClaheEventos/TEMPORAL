c9277346701c6e225d7e2826f12d62230a0e2076
ClaheEventos <personaldeit@claheeventos.com>
2026-05-22 10:50:19 -0300

    Remove admin registrations and inline classes

--- envioscocina/admin.py
+++ envioscocina/admin.py
@@ -2,52 +2,5 @@ from django.contrib import admin
 from .models import (
     Departamento, Salon, Producto, TipoProducto, 
     Envio, DetalleEnvio, StockSalon, PerfilUsuario, ConsumoSalon
-)
 
-
-class DetalleEnvioInline(admin.TabularInline):
-    model = DetalleEnvio
-    extra = 1
-
-
-@admin.register(Departamento)
-class DepartamentoAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-
-
-@admin.register(Salon)
-class SalonAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-
-
-@admin.register(TipoProducto)
-class TipoProductoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'departamento')
-
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'departamento', 'tipo', 'comportamiento_stock')
-
-@admin.register(Envio)
-class EnvioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'origen', 'destino', 'estado')
-    inlines = [DetalleEnvioInline]
-
-
-@admin.register(StockSalon)
-class StockSalonAdmin(admin.ModelAdmin):
-    list_display = ('salon', 'producto', 'cantidad')
-
-
-@admin.register(PerfilUsuario)
-class PerfilUsuarioAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'rol')
-
-
-@admin.register(ConsumoSalon)
-class ConsumoSalonAdmin(admin.ModelAdmin):
-    # ✅ CORREGIDO: usa fecha_registro en lugar de fecha
-    list_display = ('salon', 'producto', 'cantidad', 'fecha_registro', 'fecha_evento')
-    list_filter = ('salon', 'fecha_evento')
     search_fields = ('salon__nombre', 'producto__nombre')
